c1a3091c261cfd135507969b48697186744e6c9d
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-09-26 15:19:57 -0300

    Arrumar um monte de coisa

--- Downloads/cthread/src/cthread.c
+++ Downloads/cthread/src/cthread.c
@@ -3,6 +3,7 @@
 #include "../include/support.h"
 #include "../include/cdata.h"
 
+#include <ucontext.h>
 #include <stdio.h>
 
 
@@ -11,18 +12,18 @@
 int static tid=0;
 int static primeiraInit=1;
 
-FILA2 static filaAlta;
-FILA2 static filaMedia;
-FILA2 static filaBaixa;
-FILA2 static bloqueados;
-FILA2 static executando;
+FILA2  filaAlta;
+FILA2  filaMedia;
+FILA2  filaBaixa;
+FILA2  bloqueados;
+TCB_t  executando;
 
 
 PFILA2 static PfilaAlta=&filaAlta;
 PFILA2 static PfilaMedia=&filaMedia;
 PFILA2 static PfilaBaixa=&filaBaixa;
 PFILA2 static Pbloqueados=&bloqueados;
-PFILA2 static Pexecutando=&executando;
+TCB_t* Pexecutando=NULL;
 
 
 /*******************************************************************************
@@ -30,25 +31,21 @@ PFILA2 static Pexecutando=&executando;
 ********************************************************************************/
 int iniciaFilas()
 {
-  printf("Entrei na IniciaFilas \n");
 
-int e1=CreateFila2(PfilaAlta);
-printf("Criei fila alta com ponteiro \n");
-
-
-int e2=CreateFila2(PfilaMedia);
-int e3=CreateFila2(PfilaBaixa);
-int e5=CreateFila2(Pbloqueados);
-int e6=CreateFila2(Pexecutando);
+int e1=CreateFila2(&filaAlta);
+int e2=CreateFila2(&filaMedia);
+int e3=CreateFila2(&filaBaixa);
+int e5=CreateFila2(&bloqueados);
 
 
-if(e1!=0||e2!=0||e3!=0||e5!=0||e6!=0)
+if(e1!=0||e2!=0||e3!=0||e5!=0)
 return -1;
 
-  printf("Sai da IniciaFilas e: %d %d %d %d %d \n",e1,e2,e3,e5,e6);
+  printf("Sai da IniciaFilas e: %d %d %d %d \n",e1,e2,e3,e5);
 return 0;
 }
 
+TCB_t tMain;
 
 /*******************************************************************************
 
@@ -57,24 +54,16 @@ saveMain()
 {
   printf("Entrei na savemain \n");
   //Alocamos a thread
-  TCB_t *novaThread = (TCB_t*)malloc(sizeof(TCB_t));
   //Definimos a prioridade e o id da thread
-  novaThread->prio=2;
-  novaThread->tid=0;
-  novaThread->state=PROCST_CRIACAO;
+  tMain.prio=2;
+  tMain.tid=0;
+  tMain.state=PROCST_CRIACAO;
   //Obtemos o molde do contexto
-  getcontext(&novaThread->context);
-  novaThread->state=PROCST_APTO;
-  AppendFila2(&filaBaixa,novaThread);
-
-  FirstFila2(&filaBaixa);
-  TCB_t *ThreadNew = (TCB_t*)GetAtIteratorFila2(&filaBaixa);
-
-  printf("Valor da main: %d = %d --- %d = %d --- %d = %d\n ",novaThread->prio,ThreadNew->prio,novaThread->tid,ThreadNew->tid,novaThread->state,ThreadNew->state);
+  getcontext(&(tMain.context));
+  tMain.state=PROCST_APTO;
   printf("Sai da savemain \n");
 
 }
-
 
 /******************************************************************************
 Parâmetros:
@@ -107,6 +96,7 @@ int ccreate (void* (*start)(void*), void *arg, int prio)
 
   //Alocamos a thread
   TCB_t *novaThread = (TCB_t*)malloc(sizeof(TCB_t));
+
   //Definimos a prioridade e o id da thread
   novaThread->prio=prio;
   novaThread->tid=tid;
@@ -115,23 +105,24 @@ int ccreate (void* (*start)(void*), void *arg, int prio)
   //Definidos o estado inicial da Thread para criação
   novaThread->state=PROCST_CRIACAO;
 
-  printf("Thread foi criada com sucesso \n");
+
 
 
 
 
   //Obtemos o molde do contexto
-  getcontext(&novaThread->context);
+  getcontext(&(novaThread->context));
+  printf("Thread criada\n");
 
   //Alocamos uma nova stack para o contexto.
   char* stack =(char*)malloc(STACKSIZE);
   novaThread->context.uc_stack.ss_size=STACKSIZE;
   novaThread->context.uc_stack.ss_sp=stack;
   //PROVAVELMENTE SERÁ MODIFICADO O UC_LINK
-//  novaThread->context.uc_stack.uc_link=novaThread->context;
+  novaThread->context.uc_link=&(novaThread->context);
 
   //Definimos o novo contexto
-  makecontext(&novaThread->context,start,arg);
+  makecontext(&(novaThread->context),(void(*)(void))start,1,arg);
   novaThread->state=PROCST_APTO;
 
   printf("Contexto foi salvo na nova Thread \n");
@@ -146,22 +137,35 @@ int ccreate (void* (*start)(void*), void *arg, int prio)
 
   printf("Entrei no switch\n");
   //Inserimos na fila de prioridade correta
-  switch(novaThread->prio)
-  {
-  case 0:AppendFila2(PfilaAlta,novaThread);
-  break;
-  case 1:AppendFila2(PfilaMedia,novaThread);
-  break;
-  case 2:{printf("Entrei no 2\n");
-    AppendFila2(PfilaBaixa,novaThread);
-  printf("Sai do 2\n");}
-  break;
-  }
+  int inserido=inserePrioridade(novaThread);
+
   printf("Entrei na escalona\n");
   escalona();
 
+
+
 return 0;
 }
+
+int inserePrioridade(TCB_t* novaThread)
+{
+  int resultado;
+
+  switch(novaThread->prio)
+  {
+  case 0:resultado=AppendFila2(PfilaAlta,novaThread);
+  break;
+  case 1:resultado=AppendFila2(PfilaMedia,novaThread);
+  break;
+  case 2:resultado=AppendFila2(PfilaBaixa,novaThread);
+  break;
+  }
+  return resultado;
+
+
+}
+
+
 
 /******************************************************************************
 Parâmetros:
@@ -177,6 +181,45 @@ int cyield(void)
 
 }
 
+void firstThreadStart()
+{
+  TCB_t* thread = buscaFilaAlta();
+  if(thread!=NULL)
+  {
+
+    return;
+  }
+  thread=buscaFilaMedia();
+  if(thread!=NULL)
+  {
+
+    return;
+  }
+  thread=buscaFilaBaixa();
+  if(thread!=NULL)
+  {
+
+    return;
+  }
+}
+
+
+
+TCB_t* buscaFilaAlta()
+{
+
+
+}
+TCB_t* buscaFilaMedia()
+{
+
+
+}
+TCB_t* buscaFilaBaixa()
+{
+
+
+}
 
 /******************************************************************************
 Parâmetros:
@@ -187,7 +230,14 @@ Retorno:
 ******************************************************************************/
 void escalona()
 {
-  FirstFila2(&executando);
+
+
+  if(Pexecutando==NULL)
+  {
+  firstThreadStart();
+  }
+
+
   printf("Estou na escalona");
   if(FirstFila2(&executando)!=0)
   {
@@ -305,15 +355,7 @@ void escalona()
 
 
   Thread->state=PROCST_APTO;
-    switch(Thread->prio)
-    {
-    case 0:AppendFila2(&filaAlta,Thread);
-    break;
-    case 1:AppendFila2(&filaMedia,Thread);
-    break;
-    case 2:AppendFila2(&filaBaixa,Thread);
-    break;
-    }
+    inserePrioridade(Thread);
 
 }
 
